09e652ac39c04f9b6af89e1459002be6d14ebf36
Muhammad Rameez <muhammad.rameez@dgc.org>
2021-12-08 19:28:47 +0100

    added token pair to subgraph

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 A subgraph for uniswap v3 on rinkeby
 #  npm run codegen
 
-# TODO: get rid of errors when running codegen
--- src/factory.ts
+++ src/factory.ts
@@ -1,14 +1,15 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  UniswapFactory,
-  PairCreated
-} from "../generated/UniswapFactory/UniswapFactory"
-import { TokenPair } from "../generated/schema"
+  FACTORY_ADDRESS
+} from './helpers'
+import { PairCreated } from '../generated/UniswapFactory/UniswapFactory'
+import { TokenPair, UniswapFactory } from "../generated/schema"
 
 export function handlePairCreated(event: PairCreated): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
   let entity = TokenPair.load(event.transaction.from.toHex())
+  let factory = UniswapFactory.load(FACTORY_ADDRESS)
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
